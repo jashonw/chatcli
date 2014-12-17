efe85c02ea881fc1793c5aadc86227fce4bdd208
Jashon Wan <jashon_w@sina.com>
2014-12-17 14:12:06 +0800

    created --- Jashon Wan

---
+++ app.js
@@ -0,0 +1,42 @@
+//import package
+var express = require('express')
+    , path = require('path')
+    , app = express()
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server)
+    , fs = require("fs")
+    , readLine = require("readline");
+
+//set log level
+io.set('log level', 1);
+
+
+//express base config
+app.configure(function () {
+    app.set('port', process.env.PORT || 31000);
+    app.set('views', __dirname + '/views');
+    app.set('scripts', __dirname + '/scripts');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+// set client file of websocket
+app.get('/chat', function (req, res) {
+    res.sendfile('views/chat.html');
+});
+app.get('/chatcli', function (req, res) {
+    res.sendfile('views/crossdomain.html');
+});
+
+
+server.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
